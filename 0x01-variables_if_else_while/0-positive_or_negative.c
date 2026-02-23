09392f8c8937a603fa439ac3db4125603b2a7136
AKIMANA123 <axelakimana78@gmail.com>
2026-02-23 10:52:20 -0800

    Add program to print if random number is positive, negative, or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,17 @@
 #include <time.h>
 
 /**
+<<<<<<< HEAD
  * main - Entry point
+=======
+ * main - assigns a random number to n and prints if it is positive, negative or zero
+>>>>>>> Add program to print if random number is positive, negative, or zero
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+<<<<<<< HEAD
     int n;
 
     srand(time(0));
@@ -28,4 +33,19 @@ int main(void)
     }
 
     return (0);
+=======
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
+>>>>>>> Add program to print if random number is positive, negative, or zero
 }
