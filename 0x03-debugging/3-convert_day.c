4bbe7cd0db003918f5d2d31f5569cc4eb4a97f6e
AKIMANA123 <axelakimana78@gmail.com>
2026-02-28 08:53:46 -0800

    Fix convert_day and print_remaining_days functions for correct date calculations

---
+++ 0x03-debugging/3-convert_day.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+int convert_day(int month, int day)
+{
+    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int day_of_year = 0;
+    int i;
+
+    /* Sum days in previous months */
+    for (i = 0; i < month - 1; i++)
+    {
+        day_of_year += days_in_month[i];
+    }
+    
+    /* Add current day */
+    day_of_year += day;
+    
+    return (day_of_year);
+}
