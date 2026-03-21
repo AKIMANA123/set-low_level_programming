3638a9f1ce90fec8e95f6ed9d2a3435748927560
AKIMANA123 <axelakimana78@gmail.com>
2026-03-21 16:23:13 -0700

    Add static library libmy.a with all required functions

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,13 @@
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+return (&s[i]);
+i++;
+}
+if (c == '\0')
+return (&s[i]);
+return (0);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,21 @@
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+int i, j, found;
+for (i = 0; s[i] != '\0'; i++)
+{
+found = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+break;
+}
+}
+if (found == 0)
+break;
+count++;
+}
+return (count);
+}
