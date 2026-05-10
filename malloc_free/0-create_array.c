2bd59f33b45d5acfb5180908345930e5c79ff3af
AKIMANA123 <axelakimana78@gmail.com>
2026-05-10 04:57:18 -0700

    Fix Betty style: break long lines in 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,11 +2,13 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and initializes
+ * it with a specific char
  * @size: size of the array
  * @c: character to initialize the array with
  *
- * Return: pointer to the array, or NULL if size is 0 or if memory allocation fails
+ * Return: pointer to the array, or NULL if size is 0 or
+ * if memory allocation fails
  */
 char *create_array(unsigned int size, char c)
 {
