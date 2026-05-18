fbb61765b9334355ae3edd677353a652a35f1769
AKIMANA123 <axelakimana78@gmail.com>
2026-05-18 08:37:28 -0700

    Fix Betty style: ensure proper tab indentation on line 48

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * has_loop - detects if a linked list has a loop
- * @head: pointer to the head of the list
- *
- * Return: pointer to meeting point if loop exists, NULL otherwise
- */
 static const listint_t *has_loop(const listint_t *head)
 {
 const listint_t *slow = head;
@@ -23,13 +17,6 @@ return (slow);
 return (NULL);
 }
 
-/**
- * find_loop_start - finds the start node of a loop
- * @head: pointer to the head of the list
- * @meet: meeting point from Floyd's algorithm
- *
- * Return: pointer to the start of the loop
- */
 static const listint_t *find_loop_start(const listint_t *head,
 const listint_t *meet)
 {
@@ -44,12 +31,6 @@ fast = fast->next;
 return (slow);
 }
 
-/**
- * print_normal - prints a list without a loop
- * @head: pointer to the head of the list
- *
- * Return: number of nodes printed
- */
 static size_t print_normal(const listint_t *head)
 {
 size_t count = 0;
@@ -63,20 +44,12 @@ head = head->next;
 return (count);
 }
 
-/**
- * print_list_with_loop - prints a list that has a loop
- * @head: pointer to the head of the list
- * @loop_start: pointer to the start of the loop
- *
- * Return: number of nodes printed
- */
 static size_t print_list_with_loop(const listint_t *head,
-   const listint_t *loop_start)
+	const listint_t *loop_start)
 {
 const listint_t *current = head;
 size_t count = 0;
 
-/* Print nodes before the loop */
 while (current != loop_start)
 {
 printf("[%p] %d\n", (void *)current, current->n);
@@ -84,24 +57,16 @@ count++;
 current = current->next;
 }
 
-/* Print nodes in the loop */
 do {
 printf("[%p] %d\n", (void *)current, current->n);
 count++;
 current = current->next;
 } while (current != loop_start);
 
-/* Print loop indication */
 printf("-> [%p] %d\n", (void *)loop_start, loop_start->n);
 return (count);
 }
 
-/**
- * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to the head of the list
- *
- * Return: number of nodes in the list
- */
 size_t print_listint_safe(const listint_t *head)
 {
 const listint_t *meet;
