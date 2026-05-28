bbf7a9949293969f97c2a8706ab7ef4b5155f9ad
AKIMANA123 <axelakimana78@gmail.com>
2026-05-28 07:09:17 -0700

    Fix insert_dnodeint_at_index: handle insertion at index 0 correctly

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,80 +2,6 @@
 #include "lists.h"
 
 /**
- * create_node - creates a new node
- * @n: integer value to add to the new node
- *
- * Return: address of the new node, or NULL if it failed
- */
-static dlistint_t *create_node(int n)
-{
-dlistint_t *new_node;
-
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = NULL;
-
-return (new_node);
-}
-
-/**
- * insert_at_beginning - inserts a node at the beginning of the list
- * @h: double pointer to the head of the list
- * @new_node: pointer to the new node
- *
- * Return: pointer to the new node
- */
-static dlistint_t *insert_at_beginning(dlistint_t **h, dlistint_t *new_node)
-{
-new_node->next = *h;
-if (*h != NULL)
-(*h)->prev = new_node;
-*h = new_node;
-return (new_node);
-}
-
-/**
- * traverse_to_index - traverses to the node at given index
- * @head: pointer to the head of the list
- * @idx: index to traverse to
- *
- * Return: pointer to the node at index, or NULL if out of range
- */
-static dlistint_t *traverse_to_index(dlistint_t *head, unsigned int idx)
-{
-unsigned int i;
-
-for (i = 0; i < idx; i++)
-{
-if (head == NULL)
-return (NULL);
-head = head->next;
-}
-return (head);
-}
-
-/**
- * insert_before_node - inserts a new node before a given node
- * @node: pointer to the node before which to insert
- * @new_node: pointer to the new node
- *
- * Return: pointer to the new node
- */
-static dlistint_t *insert_before_node(dlistint_t *node, dlistint_t *new_node)
-{
-new_node->prev = node->prev;
-new_node->next = node;
-node->prev = new_node;
-if (new_node->prev != NULL)
-new_node->prev->next = new_node;
-return (new_node);
-}
-
-/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head of the list
  * @idx: index where the new node should be added (starts at 0)
@@ -87,23 +13,54 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new_node;
 dlistint_t *temp;
+unsigned int i;
 
 if (h == NULL)
 return (NULL);
 
-new_node = create_node(n);
+/* Create new node */
+new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
-if (idx == 0)
-return (insert_at_beginning(h, new_node));
+new_node->n = n;
 
-temp = traverse_to_index(*h, idx);
+/* Insert at beginning */
+if (idx == 0)
+{
+new_node->prev = NULL;
+new_node->next = *h;
+if (*h != NULL)
+(*h)->prev = new_node;
+*h = new_node;
+return (new_node);
+}
+
+/* Traverse to the node at the insertion point */
+temp = *h;
+for (i = 0; i < idx; i++)
+{
+if (temp == NULL)
+{
+free(new_node);
+return (NULL);
+}
+temp = temp->next;
+}
+
+/* If we reached NULL, index is beyond list length */
 if (temp == NULL)
 {
 free(new_node);
 return (NULL);
 }
 
-return (insert_before_node(temp, new_node));
+/* Insert before temp */
+new_node->prev = temp->prev;
+new_node->next = temp;
+temp->prev = new_node;
+if (new_node->prev != NULL)
+new_node->prev->next = new_node;
+
+return (new_node);
 }
